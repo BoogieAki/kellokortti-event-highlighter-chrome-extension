77c96398f1b1b9b06b4679bad78086a2e24db92f
Aki Pihlajakoski <aki.pihlajakoski@boogiesoftware.com>
2025-03-11 15:52:08 +0200

    Update README and some minor fixes

--- README.md
+++ README.md
@@ -1,23 +1,9 @@
 # kellokortti-event-highlighter-chrome-extension
 Highlight Kellokortti project events with the desired color.
 
-## Setup extension
-
-Modify `content.js`
-- Set the customer name in the `projectToFind` constant to highlight the desired project. 
-Example: `"Customer: UB Rahoitus Oy"`
-
-Modify `content.css`
-- Change the `background-color` value to your preference.
-
-You can create additional highlight functions and classes if you want to highlight multiple projects.
-
 ## How to install in Chrome
 1. Open chrome://extensions/
 2. Enable Developer mode
 3. Click "Load unpacked" and select the extension folder
 4. The extension will be loaded and will work automatically on the https://app.kellokortti.fi/app/viikko website
-
-**NOTE:** You need to refresh the extension if you make any changes.
-
-
+5. Add new projects with different colors by clicking extension icon
--- content.js
+++ content.js
@@ -1,17 +1,17 @@
 function updateEventColors(projects) {
-  document.querySelectorAll(".kellokortti_new_event").forEach(event => {
-    const innerDiv = event.querySelector(".kellokortti_new_event_inner");
-    if (!innerDiv) return;
+    document.querySelectorAll(".kellokortti_new_event").forEach(event => {
+        const innerDiv = event.querySelector(".kellokortti_new_event_inner");
+        if (!innerDiv) return;
 
-    projects.forEach(({ name, color }) => {
-        if (innerDiv.textContent.includes(name)) {
-            const barInner = event.querySelector(".kellokortti_new_event_bar_inner");
-            if (barInner) {
-                barInner.style.backgroundColor = color;
+        projects.forEach(({ name, color }) => {
+            if (innerDiv.textContent.includes(name)) {
+                const barInner = event.querySelector(".kellokortti_new_event_bar_inner");
+                if (barInner) {
+                    barInner.style.backgroundColor = color;
+                }
             }
-        }
+        });
     });
-  });
 }
 
 // Observer changes in DOM
